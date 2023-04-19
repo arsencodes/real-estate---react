138815f346a775701515adabf16fa364fd300503
Arsen <76653963+arsencodes@users.noreply.github.com>
2023-04-19 19:55:00 +0200

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ import { FiKey } from 'react-icons/fi';
 
 const Navbar = () => (
   <Flex p='2' borderBottom='1px' borderColor='gray.100'>
-    <Box fontSize='3xl' color='red.400' fontWeight='medium';>
+    <Box fontSize='2xl' color='red.400' fontWeight='medium';>
       <Link href='/' paddingLeft='2'>APPARTMENTS FOR SALE & RENT</Link>
     </Box>
     <Spacer />
